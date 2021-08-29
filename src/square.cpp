66c1e1af52a405230884d1f7bbac8a64828162cb
Yun Zou <yunzou2@gmail.com>
2021-08-29 20:25:49 +1000

    init

---
+++ src/square.cpp
@@ -0,0 +1,10 @@
+#include <ros/ros.h>
+// #include <gnc_functions.hpp>
+
+int main(int argc, char **argv) {
+  // initialize ros
+  ros::init(argc, argv, "gnc_node");
+  ros::NodeHandle gnc_node;
+
+  // Rest of code here
+}
